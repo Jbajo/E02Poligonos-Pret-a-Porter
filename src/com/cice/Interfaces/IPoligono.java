f1d1fe695824b6ebb3be7060cba70cd2091d9a29
Javier Bajo <javier.bajochacon@gmail.com>
2018-02-02 07:10:35 +0100

    Commit Protected

--- src/com/cice/Interfaces/IPoligono.java
+++ src/com/cice/Interfaces/IPoligono.java
@@ -10,16 +10,6 @@ public interface IPoligono {
     void aplicarPorcantaje();
     void mostrarPrecio();
     void descripcionProducto();
-
-    default void eliminarPorcentaje(){
-      System.out.println("Vamos a colorear el Poligono");
-    }
-
-    default boolean aniadirCoordenada(float x, float y){
-        System.out.println("Coordenada añadida");
-        return true;
-    }
-
-
-
+    void eliminarPorcentaje();
+    boolean aniadirCoordenada(float x, float y);
 }
